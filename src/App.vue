ae3439899b9f47a80ca49de5ec574a46bb924863
Oussama Alouche <theoblivionitself@gmail.com>
2024-07-23 21:37:05 +0100

    learning

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,18 @@
-import {createApp} from 'vue'
-const app =createApp({
+<template>
+  <div>
+    <Navbar />
+    <main>
+      <h1>Hello World!</h1>
+    </main>
+  </div>
+</template>
 
-})
+<script>
+import Navbar from './components/Navbar/Navbar.vue';
+
+export default {
+  components: {
+    Navbar,
+  },
+};
+</script>
---
+++ src/components/Navbar/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+  <nav>
+    <h1>My Navbar</h1>
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+  </nav>
+</template>
+
+<script setup>
+// You can add any logic or data here if needed
+</script>
+
+<style scoped>
+nav {
+  background-color: #333;
+  color: white;
+  padding: 1rem;
+}
+
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin-right: 20px;
+}
+
+a {
+  color: white;
+  text-decoration: none;
+}
+</style>
